fd1a6c48a47ff8b31934a793cdcce69541ef52d1
David Alen <davidalen.dev@gmail.com>
2023-04-17 20:46:53 -0300

    add: llvm to toy language

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
-make:
+build: source
+	make -C ./src
+
+source:
 	cmake -S . -B ./src
+
+hello: build
+	./src/tools/toy/toy ./examples/hello.toy hello
 
 clean:
 	@rm -rf src
--- tools/toy/toy.cpp
+++ tools/toy/toy.cpp
@@ -1,8 +1,22 @@
+#include <fstream>
+#include <string>
 #include <iostream>
 
-int main()
+int main(int argc,  char** argv)
 {
-  std::cout << "Hello, cpp!" << std::endl;
+  if (argc <= 2) {
+    std::cout << "usage: 'toy <path_to_file.toy>' '<path_to_output>'" << std::endl;
+    return 0;
+  }
+
+  auto filename = argv[1];
+  auto output = argv[2];
+
+  std::ifstream ifs(filename);
+  std::string content( (std::istreambuf_iterator<char>(ifs) ),
+                       (std::istreambuf_iterator<char>()    ) );
+
+  std::cout << content << std::endl;
 
   return 0;
 }
