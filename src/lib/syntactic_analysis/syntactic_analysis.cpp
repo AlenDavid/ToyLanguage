22248a1174cc8dc254b8462f05d6b2397438f6d5
David Alen <davidalen.dev@gmail.com>
2023-06-07 00:30:22 -0300

    fix: Member functions that don't mutate their objects should be declared "const"

--- src/lib/syntactic_analysis/syntactic_analysis.cpp
+++ src/lib/syntactic_analysis/syntactic_analysis.cpp
@@ -1,66 +1,62 @@
 #include <algorithm>
 #include <iostream>
-#include <string>
 #include <sstream>
+#include <string>
 
 #include "syntactic_analysis.h"
 
 using namespace tokens;
 
-namespace analysis
-{
-  SyntaxChecker::SyntaxChecker(lexical::LexicalFactory &factory) : Factory(factory) {}
+namespace analysis {
+SyntaxChecker::SyntaxChecker(lexical::LexicalFactory &factory)
+    : Factory(factory) {}
 
-  // Consume next token from Factory and assign to CurrentToken.
-  Token SyntaxChecker::Next()
-  {
-    CurrentToken = Factory.NextToken();
-    Debug("[" + From(CurrentToken) + "]");
+// Consume next token from Factory and assign to CurrentToken.
+Token SyntaxChecker::Next() {
+  CurrentToken = Factory.NextToken();
+  Debug("[" + From(CurrentToken) + "]");
 
-    return CurrentToken;
+  return CurrentToken;
+}
+
+// Will enable logs from the analysis.
+void SyntaxChecker::EnableDebug() { _Debug = true; }
+
+// Log message if Debug is true.
+void SyntaxChecker::Debug(const std::string &message) const {
+  if (_Debug) {
+    for (auto i = 0; i < NestLevel; i++)
+      std::cout << "  ";
+    std::cout << message << std::endl;
+  }
+}
+
+// Store analysis errors.
+void SyntaxChecker::Report(const std::string &expected) {
+  std::ostringstream oss;
+
+  oss << "expected: " << expected << " got: ";
+
+  if (CurrentToken == Token::tok_identifier ||
+      CurrentToken == Token::tok_string) {
+    oss << Factory.CurrentIdentifier;
   }
 
-  // Will enable logs from the analysis.
-  void SyntaxChecker::EnableDebug() { _Debug = true; }
-
-  // Log message if Debug is true.
-  void SyntaxChecker::Debug(const std::string &message)
-  {
-    if (_Debug)
-    {
-      for (auto i = 0; i < NestLevel; i++)
-        std::cout << "  ";
-      std::cout << message << std::endl;
-    }
+  else if (CurrentToken == Token::tok_double ||
+           CurrentToken == Token::tok_int) {
+    oss << Factory.CurrentNumericValue;
   }
 
-  // Store analysis errors.
-  void SyntaxChecker::Report(const std::string &expected)
-  {
-    std::ostringstream oss;
-
-    oss << "expected: " << expected << " got: ";
-
-    if (CurrentToken == Token::tok_identifier || CurrentToken == Token::tok_string)
-    {
-      oss << Factory.CurrentIdentifier;
-    }
-
-    else if (CurrentToken == Token::tok_double || CurrentToken == Token::tok_int)
-    {
-      oss << Factory.CurrentNumericValue;
-    }
-
-    else
-    {
-      oss << tokens::From(CurrentToken);
-    }
-
-    oss << " at line " << Factory.CurrentLine + 1 << ", column " << Factory.CaretPlace + 1;
-
-    Debug(oss.str());
-    Errs.emplace_back(oss.str());
-
-    oss.clear();
+  else {
+    oss << tokens::From(CurrentToken);
   }
+
+  oss << " at line " << Factory.CurrentLine + 1 << ", column "
+      << Factory.CaretPlace + 1;
+
+  Debug(oss.str());
+  Errs.emplace_back(oss.str());
+
+  oss.clear();
+}
 } // namespace analysis
--- src/lib/syntactic_analysis/syntactic_analysis.h
+++ src/lib/syntactic_analysis/syntactic_analysis.h
@@ -4,31 +4,29 @@
 #include "lib/lexical/lexical.h"
 
 #pragma once
-namespace analysis
-{
-  class SyntaxChecker
-  {
-  public:
-    int NestLevel = 0;
-    bool _Debug = false;
+namespace analysis {
+class SyntaxChecker {
+public:
+  int NestLevel = 0;
+  bool _Debug = false;
 
-    std::vector<std::string> Errs;
-    std::vector<tokens::Token> ExpressionTokens = {tokens::Token::tok_identifier, tokens::Token::tok_return};
+  std::vector<std::string> Errs;
+  std::vector<tokens::Token> ExpressionTokens = {tokens::Token::tok_identifier,
+                                                 tokens::Token::tok_return};
 
-    lexical::LexicalFactory &Factory;
-    tokens::Token CurrentToken;
+  lexical::LexicalFactory &Factory;
+  tokens::Token CurrentToken;
 
-    explicit SyntaxChecker(lexical::LexicalFactory &factory);
+  explicit SyntaxChecker(lexical::LexicalFactory &factory);
 
-    tokens::Token Next();
-    bool B();
-    bool D();
-    bool E();
-    bool G();
+  tokens::Token Next();
+  bool B();
+  bool D();
+  bool E();
+  bool G();
 
-    void EnableDebug();
-    void Debug(const std::string &);
-    void Report(const std::string &);
-  };
+  void EnableDebug();
+  void Debug(const std::string &) const;
+  void Report(const std::string &);
+};
 } // namespace analysis
-
