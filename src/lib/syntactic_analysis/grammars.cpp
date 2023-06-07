f0a2688326bf8b59fb9501214cc89f13eccf8249
David Alen <davidalen.dev@gmail.com>
2023-06-07 18:27:18 -0300

    add: format

--- src/lib/syntactic_analysis/grammars.cpp
+++ src/lib/syntactic_analysis/grammars.cpp
@@ -1,40 +1,37 @@
 #include <algorithm>
 #include <iostream>
-#include <string>
 #include <sstream>
+#include <string>
 
 #include "syntactic_analysis.h"
 
 using namespace tokens;
 
-namespace analysis
-{
-  // For grammar checking
-  bool SyntaxChecker::G()
-  {
-    NestLevel++;
-    Debug("G()");
+namespace analysis {
+// For grammar checking
+bool SyntaxChecker::G() {
+  NestLevel++;
+  Debug("G()");
 
-    for (Next(); CurrentToken != Token::tok_eof; Next())
-    {
-      if (CurrentToken == Token::tok_def && !D())
-        return false;
+  for (Next(); CurrentToken != Token::tok_eof; Next()) {
+    if (CurrentToken == Token::tok_def && !D())
+      return false;
 
-      if (std::find(ExpressionTokens.begin(), ExpressionTokens.end(), CurrentToken) != ExpressionTokens.end())
-      {
-        auto e = E();
+    // tok_identifier | tok_return
+    if (std::find(ExpressionTokens.begin(), ExpressionTokens.end(),
+                  CurrentToken) != ExpressionTokens.end()) {
+      auto e = E();
 
-        if (!e)
-        {
-          Report("expression");
-        }
-
-        return e;
+      if (!e) {
+        Report("expression");
       }
+
+      return e;
     }
-
-    NestLevel--;
-
-    return true;
   }
+
+  NestLevel--;
+
+  return true;
+}
 } // namespace analysis
