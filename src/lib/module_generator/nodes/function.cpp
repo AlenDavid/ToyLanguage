34504fbc28fcf49affed39d3b4feba76a95de628
David Alen <davidalen.dev@gmail.com>
2023-06-07 18:27:29 -0300

    add: some comments

--- src/lib/module_generator/nodes/function.cpp
+++ src/lib/module_generator/nodes/function.cpp
@@ -15,19 +15,24 @@ const std::string &FunctionAST::getName() const { return Name; }
 llvm::Function *FunctionAST::codegen(llvm::Module *Module) {
   auto Builder = llvm::IRBuilder(Module->getContext());
 
+  // TODO: handle parameters.
   std::vector<llvm::Type *> Empty(0);
 
   llvm::FunctionType *FT = llvm::FunctionType::get(
       llvm::Type::getInt32Ty(Module->getContext()), Empty, false);
 
+  // eat identifier
   llvm::Function *TheFunction = llvm::Function::Create(
       FT, llvm::Function::ExternalLinkage, getName(), *Module);
 
+  // eat {
   llvm::BasicBlock *BB =
-      llvm::BasicBlock::Create(Module->getContext(), "entry", TheFunction);
+      llvm::BasicBlock::Create(Module->getContext(), "block", TheFunction);
 
+  // eat G()
   Builder.SetInsertPoint(BB);
 
+  // eat return
   if (llvm::Value *RetVal = llvm::ConstantInt::get(
           llvm::Type::getInt32Ty(Module->getContext()), 5)) {
 
