39e9ddfadd568b14d0e33c87e8e440d558170986
David Alen <davidalen.dev@gmail.com>
2023-06-09 11:07:53 -0300

    refactor: move message handling to its own file

--- src/lib/syntactic_analysis/syntactic_analysis.cpp
+++ src/lib/syntactic_analysis/syntactic_analysis.cpp
@@ -27,50 +27,6 @@ Token SyntaxChecker::Next() {
   return CurrentToken;
 }
 
-// Will enable logs from the analysis.
-void SyntaxChecker::EnableDebug() { _Debug = true; }
-
-// Log message if Debug is true.
-void SyntaxChecker::Debug(const std::string &message) const {
-  if (_Debug) {
-    for (auto i = 0; i < NestLevel; i++)
-      llvm::dbgs() << "  ";
-    llvm::dbgs() << message << "\n";
-  }
-}
-
-std::nullptr_t SyntaxChecker::Error(const std::string &message) {
-  NestLevel--;
-  llvm::errs() << message << " at line " << Factory.CurrentLine + 1
-               << ", column " << Factory.CaretPlace + 1 << "\n";
-  return nullptr;
-}
-
-std::nullptr_t SyntaxChecker::Expect(const std::string &expected) {
-  NestLevel--;
-  llvm::errs() << "expected: " << expected << " got: ";
-
-  if (CurrentToken == Token::tok_identifier ||
-      CurrentToken == Token::tok_string) {
-    llvm::errs() << Factory.CurrentIdentifier;
-  }
-
-  else if (CurrentToken == Token::tok_double)
-    llvm::errs() << Factory.CurrentFloatValue;
-
-  else if (CurrentToken == Token::tok_int)
-    llvm::errs() << Factory.CurrentIntValue;
-
-  else {
-    llvm::errs() << tokens::From(CurrentToken);
-  }
-
-  llvm::errs() << " at line " << Factory.CurrentLine + 1 << ", column "
-               << Factory.CaretPlace + 1 << "\n";
-
-  return nullptr;
-}
-
 llvm::Expected<std::unique_ptr<SyntaxChecker>> SyntaxChecker::Codegen() {
   while (Next() != Token::tok_eof) {
     if (!G())
