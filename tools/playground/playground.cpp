3df7ff9e67fc51e76d390470cd2e47350337044e
David Alen <davidalen.dev@gmail.com>
2023-06-08 21:17:23 -0300

    add: FOUND IT

--- tools/playground/playground.cpp
+++ tools/playground/playground.cpp
@@ -1,19 +1,36 @@
+#include "llvm/IR/Constants.h"
 #include "llvm/IR/DerivedTypes.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Type.h"
+#include "llvm/Support/TargetSelect.h"
 #include <iostream>
 #include <memory>
 #include <string>
 
+#include "lib/module_emmiter/module_emmiter.h"
+
+using namespace std;
 using namespace llvm;
 
 int main() {
+  llvm::InitializeNativeTarget();
+  llvm::InitializeNativeTargetAsmPrinter();
+  llvm::InitializeNativeTargetAsmParser();
+  llvm::InitializeAllTargetInfos();
+  llvm::InitializeAllTargets();
+  llvm::InitializeAllTargetMCs();
+  llvm::InitializeAllAsmParsers();
+  llvm::InitializeAllAsmPrinters();
+
   auto ctx = llvm::LLVMContext();
 
-  llvm::Module Module("playground", ctx);
-  IRBuilder<> Builder(ctx);
+  unique_ptr<llvm::Module> Module;
+  unique_ptr<llvm::IRBuilder<>> Builder;
+
+  Module = make_unique<llvm::Module>("playground", ctx);
+  Builder = make_unique<llvm::IRBuilder<>>(ctx);
 
   std::vector<llvm::Type *> Empty(0);
 
@@ -21,11 +38,29 @@ int main() {
       llvm::FunctionType::get(llvm::Type::getInt32Ty(ctx), Empty, false);
 
   llvm::Function *TheFunction = llvm::Function::Create(
-      FT, llvm::Function::ExternalLinkage, "__entry", Module);
+      FT, llvm::Function::ExternalLinkage, "main", Module.get());
 
   llvm::BasicBlock *BB = llvm::BasicBlock::Create(ctx, "entry", TheFunction);
 
-  Builder.SetInsertPoint(BB);
+  cout << "SetInsertPoint" << endl;
+  Builder->SetInsertPoint(BB);
+  Value *v = ConstantInt::get(Type::getInt32Ty(ctx), 5);
+
+  cout << "CreateAlloca" << endl;
+  auto *A = Builder->CreateAlloca(v->getType(), nullptr, "a");
+
+  cout << "CreateStore" << endl;
+  auto store = Builder->CreateStore(v, A, false);
+
+  Builder->CreateRet(v);
+
+  cout << "Pass" << endl;
+
+  if (Module.get()) {
+    cout << Module.get()->getName().str() << endl;
+  }
+
+  run_pass_on_module(Module.get(), "playground.o");
 
   return 0;
 }
